eb92997c5b42399bf625edc6ec437a42b817801a
rickyelqasem <ricky@djsho.co.uk>
2016-12-02 08:28:11 +0000

    change

--- app.go
+++ app.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	owm "github.com/briandowns/openweathermap"
 	"log"
 	"net/http"
 	"os"
@@ -11,25 +10,11 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 
-	we, err := owm.NewCurrent("C", "en")
-	if err != nil {
-		log.Fatalln(err)
-	}
 
-	we.CurrentByName("London")
-
-	s := fmt.Sprint(we.Weather)
-
-	strArray := strings.Fields(s)
-
-	var weather string = string(strArray[2])
-	weather = weather + " " + string(strArray[3])
-	//var weather string = getWeather()
-	fmt.Fprintf(w, "Hi there Ricky, the weather in London is "+ weather +" %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hi there Ricky, the weather in London is meh!", r.URL.Path[1:])
 }
 
 func main() {
-	os.Setenv("OWM_API_KEY", "c0ce4a2db13e8061f06fc7a77db850f1")
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
